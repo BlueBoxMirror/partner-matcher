36797ab0e92d0b9d8e9eb2c574ecf87b42e8775e
hanjianing2544 <2440449946@qq.com>
2026-05-26 17:10:25 +0800

    Login blocking authorization and sop log

---
+++ partner-matching-security/src/main/java/com/partner/entity/ApiCallLog.java
@@ -0,0 +1,33 @@
+package com.partner.entity;
+
+import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.annotation.TableId;
+import com.baomidou.mybatisplus.annotation.TableName;
+import lombok.Data;
+
+import java.time.LocalDateTime;
+
+@Data
+@TableName("api_log")
+public class ApiCallLog {
+    @TableId(type = IdType.AUTO)
+    private Long id;
+
+
+    private Long userId;
+
+
+    private String apiName;
+
+
+    private String requestParams;
+
+
+    private String responseResult;
+
+
+    private Long costTime;
+
+
+    private LocalDateTime createTime;
+}
